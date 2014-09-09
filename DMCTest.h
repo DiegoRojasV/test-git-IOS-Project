724b5b3404cfe55d99fcb147773f5968028a120a
Diego Rojas <diego@d-mentecreativa.com>
2014-09-10 01:28:21 +0200

    se añadió un clase

---
+++ DMCTest.h
@@ -0,0 +1,13 @@
+//
+//  DMCTest.h
+//  Git Test
+//
+//  Created by  on 10/09/14.
+//  Copyright (c) 2014 D-mente Creativa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMCTest : NSObject
+
+@end
---
+++ DMCTest.m
@@ -0,0 +1,13 @@
+//
+//  DMCTest.m
+//  Git Test
+//
+//  Created by  on 10/09/14.
+//  Copyright (c) 2014 D-mente Creativa. All rights reserved.
+//
+
+#import "DMCTest.h"
+
+@implementation DMCTest
+
+@end
